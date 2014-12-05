0c69ec22624c732a92b37a4f19cf8601803ad14e
Chris Dinger <ding0057@umn.edu>
2014-12-05 16:12:26 -0600

    Simplify test database checking

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,21 +5,18 @@ $:.unshift(File.expand_path('../../lib', __FILE__))
 require 'activerecord-peoplesoft_models'
 require 'minitest/autorun'
 
-DATABASE = ENV['DATABASE'] || 'sqlite'
-
-if DATABASE == 'sqlite'
+if ENV['DATABASE'].blank?
   ActiveRecord::Base.establish_connection(adapter:"sqlite3", database: ":memory:")
-
   require 'fake_peoplesoft'
   FakePeoplesoft.create!
 else
   ActiveRecord::Base.configurations = YAML::load_file(File.join(File.dirname(__FILE__), 'config', 'database.yml'))
-  ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[DATABASE])
+  ActiveRecord::Base.establish_connection(ActiveRecord::Base.configurations[ENV['DATABASE']])
 end
 
 class Minitest::Test
   def skip_if_using_real_peoplsoft
-    unless DATABASE == 'sqlite'
+    unless ENV['DATABASE'].blank?
       skip "Not running this destructive test on a real PeopleSoft instance"
     end
   end
